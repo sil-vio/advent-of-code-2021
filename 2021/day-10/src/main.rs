919eb935f7c95989ba3d1404fb1d403b2b4b0283
Silvio  Giannini <giannini.silvio@gmail.com>
2021-12-10 10:55:24 +0100

    day 10 part 2

--- 2021/day-10/src/main.rs
+++ 2021/day-10/src/main.rs
@@ -1,52 +1,79 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Lines};
+use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
 fn main() {
     let lines = lines_from_file("input").unwrap();
-    let result_part_1 = part1(&lines);
-    println!("part 1 : {}", result_part_1);
+    let result = part1(&lines);
+    println!("part 1 : {}", result.0);
+    println!("part 2 : {}", result.1);
 }
 
-fn part1(lines: &Vec<Vec<char>>) -> u64 {
-    let mut result = 0;
+fn part1(lines: &Vec<Vec<char>>) -> (u64, u64) {
+    let mut result_1 = 0;
     let charmap: HashMap<char, char> =
         HashMap::from([('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]);
     let mut error_map: HashMap<char, u64> = HashMap::from([(')', 0), ('>', 0), ('}', 0), (']', 0)]);
-    let point_map: HashMap<char, u64> = HashMap::from([(')', 3), ('>', 25137), ('}', 1197), (']', 57)]);
+    let part1_point_map: HashMap<char, u64> =
+        HashMap::from([(')', 3), ('>', 25137), ('}', 1197), (']', 57)]);
+
+    let mut array_score: Vec<u64> = Vec::new();
 
     for line in lines {
         let mut stack = Vec::new();
-        // if line.len() % 2 == 0 {
-            for c in line {
-                match charmap.get(c) {
-                    Some(_) => stack.push(c),
-                    None => match stack.last() {
-                        Some(last_char) => {
-                            if charmap.get(&last_char).unwrap() == c {
-                                stack.pop();
-                            } else {
-                                let value = error_map.entry(*c).or_insert(0);
-                                *value += 1;
-                                break;
-                            }
-                        }
-                        None => {
+        let mut corrupted = false;
+        for c in line {
+            match charmap.get(c) {
+                Some(_) => stack.push(c),
+                None => match stack.last() {
+                    Some(last_char) => {
+                        if charmap.get(&last_char).unwrap() == c {
+                            stack.pop();
+                        } else {
                             let value = error_map.entry(*c).or_insert(0);
                             *value += 1;
+                            corrupted = true;
                             break;
                         }
-                    },
+                    }
+                    None => {
+                        let value = error_map.entry(*c).or_insert(0);
+                        *value += 1;
+                        corrupted = true;
+                        break;
+                    }
+                },
+            }
+        }
+        if !corrupted && stack.len() > 0 {
+            let mut closing_sequence = Vec::new();
+            stack.reverse();
+            for c in stack {
+                match charmap.get(c) {
+                    Some(inverse_c) => closing_sequence.push(inverse_c),
+                    None => panic!("errore carattere non trovato {}", c),
                 }
             }
-        //}
+            array_score.push(closing_sequence_score(closing_sequence));
+        }
     }
     for entry in error_map {
-        let coefficent = point_map.get(&entry.0).unwrap();
-        result += entry.1 * coefficent;
+        let coefficent = part1_point_map.get(&entry.0).unwrap();
+        result_1 += entry.1 * coefficent;
     }
+    array_score.sort();
+    (result_1, array_score[(array_score.len() / 2 ) as usize])
+}
 
+fn closing_sequence_score(closing_sequence: Vec<&char>) -> u64 {
+    let mut result = 0;
+    let part2_point_map: HashMap<char, u64> =
+        HashMap::from([(')', 1), ('>', 4), ('}', 3), (']', 2)]);
+    for c in closing_sequence {
+        result = result * 5;
+        result += part2_point_map.get(c).unwrap();
+    }
     result
 }
 
@@ -62,11 +89,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parsing_part1() {
+    fn test_day_9() {
         let lines = lines_from_file("test_input").unwrap();
         assert_eq!(lines.len(), 10);
         assert_eq!(lines[0].len(), 24);
         let result = part1(&lines);
-        assert_eq!(result, 26397);
+        assert_eq!(result.0, 26397);
+        assert_eq!(result.1, 288957);
+
+    }
+
+
+    #[test]
+    fn test_closing_sequence_score() {
+        let input_1 = vec![&'}', &'}', &']', &']', &')', &'}', &')',&']'];
+        assert_eq!(closing_sequence_score(input_1), 288957);
     }
 }
