12d6ea929efa922f3bddd00364014f32960f3715
silvio <giannini.silvio@gmail.com>
2021-12-11 10:23:17 +0100

    day 11

---
+++ 2021/day-11/src/main.rs
@@ -0,0 +1,116 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Point {
+    x: usize,
+    y: usize,
+    value: u64,
+}
+
+fn main() {
+    let mut input = lines_from_file("input").unwrap();
+    let (p1, p2) = part1(&mut input);
+    println!("part 1 result: {}, part2 result {} ", p1, p2 );
+}
+
+fn part1(input: &mut Vec<Vec<u64>>) -> (u64, usize) {
+    let mut flash_counter = 0;
+    let mut first_step_all_flash = 0;
+    println!("\n day 0");
+    for y in 0..input.len() {
+        println!("{:?}", input[y]);
+    }
+    let mut step = 1;
+    loop {
+        for y in 0..input.len() {
+            for x in 0..input[y].len() {
+                input[y][x] += 1;
+                if input[y][x] == 10 {
+                    flash_point(input, x, y);
+                }
+            }
+        }
+        for y in 0..input.len() {
+            for x in 0..input[y].len() {
+                if input[y][x] > 9 {
+                    input[y][x] = 0;
+                    if step <= 100 {
+                        flash_counter += 1;
+                    }
+                }
+            }
+        }
+        if first_step_all_flash == 0 {
+            let mut day_sum = 0;
+            for y in 0..input.len() {
+                day_sum = day_sum + input[y].iter().sum::<u64>();
+            }
+            if day_sum == 0 {
+                first_step_all_flash = step;
+                break
+            }
+        }
+        step += 1;
+    }
+    (flash_counter, first_step_all_flash)
+}
+
+fn flash_point(input: &mut Vec<Vec<u64>>, x: usize, y: usize) {
+    let mut y_near = Vec::new();
+    if y > 0 {
+        y_near.push(y - 1);
+    }
+    if y < input.len() - 1 {
+        y_near.push(y + 1);
+    }
+    y_near.push(y);
+    let mut x_near = Vec::new();
+    if x > 0 {
+        x_near.push(x - 1);
+    }
+    if x < input[y].len() - 1 {
+        x_near.push(x + 1);
+    }
+    x_near.push(x);
+    for near_y in &y_near {
+        for near_x in &x_near {
+            if !(*near_x == x && *near_y == y) && input[*near_y][*near_x] < 10 {
+                input[*near_y][*near_x] += 1;
+                if input[*near_y][*near_x] == 10 {
+                    flash_point(input, *near_x, *near_y);
+                }
+            }
+        }
+    }
+}
+
+fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<Vec<u64>>> {
+    Ok(BufReader::new(File::open(filename)?)
+        .lines()
+        .map(|s| {
+            s.unwrap()
+                .chars()
+                .map(|c| c.to_string().parse::<u64>().unwrap())
+                .collect()
+        })
+        .collect::<Vec<Vec<u64>>>())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parsing_part1() {
+        let mut lines = lines_from_file("test_input").unwrap();
+        assert_eq!(lines.len(), 10);
+        for line in &lines {
+            assert_eq!(line.len(), 10);
+        }
+        let (p1, p2) = part1(&mut lines);
+        assert_eq!(p1, 1656);
+        assert_eq!(p2, 195)
+    }
+}
