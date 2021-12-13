6e0dd638cec2faa4670ad122f4a5f0bc66b04b69
Silvio  Giannini <giannini.silvio@gmail.com>
2021-12-13 14:48:40 +0100

    2021 day 13

--- 2021/day-13/src/main.rs
+++ 2021/day-13/src/main.rs
@@ -1,10 +1,28 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::num::ParseIntError;
 use std::path::Path;
 use std::str::FromStr;
 use std::time::Instant;
-use std::num::ParseIntError;
 
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Fold {
+    x: bool,
+    value: usize,
+}
+
+impl FromStr for Fold {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s_replaced = s.replace("fold along ", "");
+        let (x, value) = s_replaced.split_once('=').unwrap_or(("a", "a"));
+        Ok(Fold {
+            x: x == "x",
+            value: value.parse::<usize>()?,
+        })
+    }
+}
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Point {
@@ -16,18 +34,20 @@ impl FromStr for Point {
     type Err = ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (x,y) = s.split_once(',').unwrap_or(("a", "a"));
-        Ok(Point{
+        let (x, y) = s.split_once(',').unwrap_or(("a", "a"));
+        Ok(Point {
             x: x.parse::<usize>()?,
-            y: y.parse::<usize>()?
+            y: y.parse::<usize>()?,
         })
     }
 }
 
 fn main() {
-    let mut input = lines_from_file("input").unwrap();
+    let mut points = get_points_from_file("input").unwrap();
+    let mut folds = get_folds_from_file("input").unwrap();
     let now = Instant::now();
-    let (p1, p2) = part1(&mut input);
+    let p1 = part1(&points, &folds);
+    let p2 = 0;
     println!(
         "part 1 result: {}, part2 result {} in ns {}",
         p1,
@@ -36,21 +56,79 @@ fn main() {
     );
 }
 
-fn part1(input: &mut Vec<Point>) -> (u64, usize) {
-    let mut flash_counter = 0;
-    let mut step = 1;
-    (flash_counter, step)
+fn part1(points: &Vec<Point>, folds: &Vec<Fold>) -> usize {
+    let mut counter = 0;
+    let mut rows = vec![vec![0; 1311]; 895];
+    for point in points {
+        rows[point.y][point.x] = 1;
+    }
+    // print_rows(&rows);
+    for fold in folds {
+        println!("execute fold : {:?}", &fold);
+        if fold.x {
+            let mut new_rows = vec![vec![0; rows[0].len()/2]; rows.len()];
+            for y in 0..new_rows.len() {
+                for x in 0..new_rows[y].len() {
+                    new_rows[y][x] = if (rows[y][x] + rows[y][(rows[y].len() - 1) - x]) > 0 { 1 } else { 0 };
+                }
+            }
+            rows = new_rows;
+        } else {
+            let mut new_rows = vec![vec![0; rows[0].len()]; rows.len()/2];
+            for y in 0..new_rows.len() {
+                for x in 0..new_rows[y].len() {
+                    new_rows[y][x] = if (rows[y][x] + rows[(rows.len() - 1) - y][x]) > 0 { 1 } else { 0 };
+                }
+            }
+            rows = new_rows;
+        }
+        println!("dots #: {}", counts_dots(&rows));
+    }
+    print_rows(&rows);
+    counter
 }
 
+fn print_rows(rows: &Vec<Vec<i32>>) {
+    for row in rows {
+        print!("\n");
+        for cell in row {
+            print!("{}", if *cell != 1i32 { '.' } else { '#'});
+        }
+    }
+}
 
-fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<Point>> {
+fn counts_dots(rows: &Vec<Vec<i32>>) -> usize {
+    let mut counter = 0;
+     for row in rows {
+        for cell in row {
+            if *cell > 0 {
+                counter += 1;
+            }
+        }
+    }
+    counter
+}
+
+fn get_points_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<Point>> {
     let file = File::open(filename)?;
-    let points: Vec<Point> = io::BufReader::new(file).lines()
+    let points: Vec<Point> = io::BufReader::new(file)
+        .lines()
         .map(|line| Point::from_str(line.unwrap().as_str()))
         .filter(|l| l.is_ok())
         .map(|l| l.unwrap())
-    .collect();
+        .collect();
     Ok(points)
+}
+
+fn get_folds_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<Fold>> {
+    let file = File::open(filename)?;
+    let folds: Vec<Fold> = io::BufReader::new(file)
+        .lines()
+        .map(|line| Fold::from_str(line.unwrap().as_str()))
+        .filter(|l| l.is_ok())
+        .map(|l| l.unwrap())
+        .collect();
+    Ok(folds)
 }
 
 #[cfg(test)]
@@ -59,7 +137,10 @@ mod tests {
 
     #[test]
     fn test_parsing_part1() {
-        let  points = lines_from_file("test_input").unwrap();
+        let points = get_points_from_file("test_input").unwrap();
+        let folds = get_folds_from_file("test_input").unwrap();
         assert_eq!(points.len(), 18);
+        assert_eq!(folds.len(), 2);
+        assert_eq!(part1(&points, &folds), 17);
     }
 }
