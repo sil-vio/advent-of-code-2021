fe54447e51d873e20d64c8f1d69c865ef8ac32e4
Silvio  Giannini <giannini.silvio@gmail.com>
2021-12-15 15:27:25 +0100

    2021 day 15 refactor

--- 2021/day-15/src/main.rs
+++ 2021/day-15/src/main.rs
@@ -6,55 +6,54 @@ use std::usize;
 
 fn main() {
     let tile = lines_from_file("input").unwrap();
-    println!("min dist to exit : {}", part1(&tile));
+    println!("min dist to exit : {}", part1(&tile,1));
     let complete_map:Vec<Vec<u64>> = create_map(&tile);
-    println!("min dist to exit : {}", part1(&complete_map));
+    println!("min dist to exit : {}", part1(&complete_map, 1));
 
 }
 
-fn part1(input: &Vec<Vec<u64>>) -> u64 {
-    let mut dist: Vec<Vec<u64>> = vec![vec![u64::MAX; input[0].len()]; input.len()];
+fn part1(input: &Vec<Vec<u64>>, map_size: usize) -> u64 {
+    // let mut dist: Vec<Vec<u64>> = vec![vec![u64::MAX; input[0].len() * map_size]; input.len() * map_size];
     let mut map: HashMap<(usize, usize), u64> = HashMap::new();
     map.insert((0, 0), input[0][0]);
     let  (mut y, mut x): (usize, usize) = (0,0);
     let max_x = input[0].len();
     let max_y = input.len();
-    while y < max_y {
-        while x < max_x {
+    while y < max_y * map_size {
+        while x < max_x * map_size{
             let dist_to_point = map.get(&(y, x)).unwrap().clone();
-            for (ny, nx) in get_neighbours(y, x, max_y, max_x) {
+            for (ny, nx) in get_neighbours(y, x, max_y, max_x, map_size) {
                 let dist_to_neighbour = map.entry((ny, nx)).or_insert(u64::MAX);
                 let new_dist_neighbour: u64 = dist_to_point + get_value_from_map(input, ny, nx);
                 if *dist_to_neighbour > new_dist_neighbour {
                     *dist_to_neighbour = new_dist_neighbour;
-                    dist[ny][nx] = new_dist_neighbour;
                     if ny < y || nx < x {
                         y = y.min(ny);
                         x = x.min(nx - 1);
                     }
-                }
+                } 
             }
             x += 1;
         }
         x = 0;
         y += 1;
     }
-    dist[max_y - 1][max_x - 1] - input[0][0]
+    map.get(&(max_y* map_size -1 , max_x* map_size -1)).unwrap() - input[0][0]
 }
 
 
-fn get_neighbours(y: usize, x: usize, len_y: usize, len_x: usize) -> Vec<(usize, usize)> {
+fn get_neighbours(y: usize, x: usize, len_y: usize, len_x: usize, map_size: usize) -> Vec<(usize, usize)> {
     let mut neighbours = Vec::new();
     if y > 0 {
         neighbours.push((y - 1, x));
     }
-    if y < len_y - 1 {
+    if y < (len_y * map_size) - 1 {
         neighbours.push((y + 1, x));
     }
     if x > 0 {
         neighbours.push((y, x - 1));
     }
-    if x < len_x - 1{
+    if x < (len_x * map_size) - 1{
         neighbours.push((y, x + 1));
     }
     neighbours
@@ -87,7 +86,7 @@ fn get_value_from_map(tile:  &Vec<Vec<u64>>, y:usize, x:usize) -> u64 {
     let mod_x = x % tile[0].len();
     let plus_y = if y >= tile.len() { y / tile.len() } else { 0 };
     let plus_x = if x >= tile[0].len() { x / tile.len() } else { 0 };          
-    let new_value = (tile[mod_y][mod_x] + plus_x as u64 + plus_y as u64);
+    let new_value = tile[mod_y][mod_x] + plus_x as u64 + plus_y as u64;
     return  if new_value > 9 { new_value % 9 } else { new_value };
 }
 
@@ -107,8 +106,9 @@ mod tests {
             assert_eq!(line.len(), 10);
         }
         assert_eq!(tile[2][2], 3);
-        assert_eq!(part1(&tile), 40);
-        assert_eq!(part1(&complete_map), 315);
+        assert_eq!(part1(&tile, 1), 40);
+        assert_eq!(part1(&complete_map, 1), 315);
+        assert_eq!(part1(&tile, 5), 315);
 
     }
 
